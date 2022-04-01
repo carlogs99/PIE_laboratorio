0f61b202c264898db263726c401c58cf764aff3c
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-04-01 14:04:09 -0300

    Added 4.4 and 4.5

--- Obligatorio1/bits.c
+++ Obligatorio1/bits.c
@@ -37,6 +37,27 @@ unsigned int set_bit(unsigned int buffer, int pos, int valor){
 	return buffer;
 }
 
+// Devuelve una mascara que vale 1 en el rango pos_ms >= x >= pos_ls
+// y cero en los demas bits
+// Se supone 31 >= pos_ms >= pos_ls >= 0
+unsigned int crear_mascara(int pos_ls, int pos_ms){
+	unsigned int mascara = 0;
+	for(int i = pos_ls ; i <= pos_ms ; i++){
+		mascara = set_bit(mascara, i, 1);
+	}
+	
+	return mascara;
+}
+
+unsigned int concatena(unsigned int buffer_ms, unsigned int buffer_ls, int num_bits_ls){
+	unsigned int mask_ls = crear_mascara(0, num_bits_ls - 1);
+	 
+	buffer_ms = buffer_ms << num_bits_ls;
+	buffer_ls = buffer_ls & mask_ls;
+	
+	return (buffer_ms | buffer_ls);
+}
+
 // Devuelve base ^ exponent
 int power(int base, unsigned int exponent){
 	int result = 1;
--- Obligatorio1/obligatorio1.c
+++ Obligatorio1/obligatorio1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "bits.h"
 
+// Para los testeos no se planea agregar chequeo de la entrada
+// el correcto funcionamiento de los tests queda a merced del tester
 int main(int argc, char** argv){
 	/* test: bit
 	unsigned int buffer = atoi(argv[1]);
@@ -18,11 +20,25 @@ int main(int argc, char** argv){
 	return 0;
 	*/
 	
-	/* test: set_bit */
+	/* test: set_bit 
 	unsigned int buffer = atoi(argv[1]);
 	int pos = atoi(argv[2]);
 	int valor = atoi(argv[3]);
 	ver_binario(buffer, 0, 31);
 	ver_binario(set_bit(buffer, pos, valor), 0 , 31);
+	return 0;
+	*/
+	
+	/* test: crear_mascara 
+	int pos_ls = atoi(argv[1]);
+	int pos_ms = atoi(argv[2]);
+	ver_binario(crear_mascara(pos_ls, pos_ms), 0, 31);
+	return 0;
+	*/
+	
+	/* test: concatena */
+	unsigned int buffer_ms = 629145;
+	unsigned int buffer_ls = 26214;
+	ver_binario(concatena(buffer_ms, buffer_ls, 10), 0, 31);
 	return 0;
 }
