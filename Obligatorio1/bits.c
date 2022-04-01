b3f395e726d78c82ecc5b3e3ff3b18ba47af4749
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-04-01 07:31:08 -0300

    Added first three functions and all function declarations to header file

--- Obligatorio1/bits.c
+++ Obligatorio1/bits.c
@@ -1,1 +1,48 @@
-..
+#include <stdio.h>
+#include <limits.h>
+#include "bits.h"
+
+// Las posiciones se numeran como: MSB: ... b_7 b_6 ... b_0 :LSB
+
+// Devuelve el valor del bit de buffer en la posicion pos_bit
+int bit(unsigned int buffer, int pos_bit){
+	for(int i = 0 ; i < pos_bit ; i++){
+		buffer = buffer >> 1;
+	}
+	
+	return (buffer & 1);
+}
+
+// Imprime el los bits de buffer entre pos_ls y pos_ms (inclusive ambos)
+// Se supone 31 >= pos_ms >= pos_ls >= 0
+void ver_binario(unsigned int buffer, int pos_ls, int pos_ms){
+	for(int i = pos_ms ; i >= pos_ls ; i--){
+		printf("%d", bit(buffer, i));
+	}
+	printf("\n");
+	
+	return;
+}
+
+// Setea el bit de buffer en posicion pos a valor 
+// Se supone que valor siempre vale 0 o 1
+unsigned int set_bit(unsigned int buffer, int pos, int valor){
+	if(valor){
+		buffer = buffer | power(2, pos); 
+	}
+	else{
+		buffer = buffer & (UINT_MAX - power(2, pos));
+	}
+	
+	return buffer;
+}
+
+// Devuelve base ^ exponent
+int power(int base, unsigned int exponent){
+	int result = 1;
+	for (int i = 0 ; i < exponent ; i++){
+		result *= base;
+	}
+	
+	return result;
+}
--- Obligatorio1/obligatorio1.c
+++ Obligatorio1/obligatorio1.c
@@ -1,1 +1,28 @@
-..
+#include <stdio.h>
+#include <stdlib.h>
+#include "bits.h"
+
+int main(int argc, char** argv){
+	/* test: bit
+	unsigned int buffer = atoi(argv[1]);
+	int pos_bit = atoi(argv[2]);
+	printf("%d\n", bit(buffer, pos_bit));
+	return 0;
+	*/
+	
+	/* test: ver_binario 
+	unsigned int buffer = atoi(argv[1]);
+	int pos_ls = atoi(argv[2]);
+	int pos_ms = atoi(argv[3]);
+	ver_binario(buffer, pos_ls, pos_ms);
+	return 0;
+	*/
+	
+	/* test: set_bit */
+	unsigned int buffer = atoi(argv[1]);
+	int pos = atoi(argv[2]);
+	int valor = atoi(argv[3]);
+	ver_binario(buffer, 0, 31);
+	ver_binario(set_bit(buffer, pos, valor), 0 , 31);
+	return 0;
+}
