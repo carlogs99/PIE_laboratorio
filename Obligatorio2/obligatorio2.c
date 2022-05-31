119075e5f67be065e85ff768085351a4dc2aed67
carlosgruss99@gmail.com <carlosgruss99@gmail.com>
2022-05-30 21:11:40 -0300

    Finished subcomando convertir_formato

--- Obligatorio2/obligatorio2.c
+++ Obligatorio2/obligatorio2.c
@@ -16,52 +16,19 @@
 
 int main(int argc, char** argv) {
 	Imagen_t pin;
-	CodigoError_t CE; 
+	CodigoError_t CE;
+	FormatoPPM_t formato; 
 	
-	/*Tests basicos inicializar_imagen y destruir_imagen
-	Imagen_t pin;
-	CodigoError_t codigoIni = inicializar_imagen(&pin, 10, 12);
-	printf("CodigoError inicializar_imagen: %d\n", codigoIni);
-	printf("nFilas = %d, nCol = %d\n", pin.filas, pin.columnas);
-	for(int i = 0 ; i < 10 ; i++) {
-		for(int j = 0 ; j < 12 ; j++) {
-			printf("Contenido en IMG(%d, %d) = %d\n", i, j, (pin.pixeles)[i][j]); 
-		}
-	}
-	
-	CodigoError_t codigoDest = destruir_imagen(&pin);
-	printf("CodigoError destruir_imagen: %d\n", codigoDest);
-	*/
-	
-	/*Test para leer_imagen y escribir_imagen 
-	Imagen_t pin;
-	CodigoError_t CE = leer_imagen("./imagenes_ppm/lagartija_plano.ppm", &pin);
-	printf("CodigoError leer_imagen: %d\n", CE);
-	for(int i = 0 ; i < pin.filas ; i++) {
-		for(int j = 0 ; j < pin.columnas ; j++) {
-			printf("Contenido en IMG(%d, %d) = %u\n", i, j, (pin.pixeles)[i][j]); 
-		}
-	}
-	
-	CE = escribir_imagen(&pin, "./lagartija_plano_copy.ppm", NO_PLANO);
-	printf("CodigoError escribir_imagen: %d\n", CE);
-	
-	CE = destruir_imagen(&pin);
-	printf("CodigoError destruir_imagen: %d\n", CE);
-	*/
-	
-	//Subcomando: convertir_formato
-	//Subcomando para convertir de formato plano a no plano y viceversa.
-	//Formato: ./obligatorio2 convertir_formato RUTA_IM_ENTRADA RUTA_IM_SALIDA FORMATO_PPM
 	if(!(strcmp(argv[1], "convertir_formato"))){
-		if(argc != 5) {
+		if(argc != 5 || (strcmp(argv[4], "PLANO") && strcmp(argv[4], "NO_PLANO"))) {
 			return ERROR;
 		} else {
 			CE = leer_imagen(argv[2], &pin);
 			if(CE != OK) {
 				return CE;
 			} else {
-				CE = escribir_imagen(&pin, argv[3], PLANO);
+				formato = !strcmp(argv[4], "PLANO") ? PLANO : NO_PLANO;
+				CE = escribir_imagen(&pin, argv[3], formato);
 				if(CE != OK) {
 					return CE;
 				} else {
@@ -73,7 +40,6 @@ int main(int argc, char** argv) {
 	
 	
 	
-	
-	return OK;
+	return ERROR;
 }
 
